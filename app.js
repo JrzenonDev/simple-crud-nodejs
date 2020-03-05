9a46ab4d9b1df1f3fbfad5c3daac093ffcb716f7
JrzenonDev <jrobertoonb@gmail.com>
2020-03-04 23:46:20 -0300

    chore: iniciando o servidor

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const handleBars = require('express-handlebars');
+
+const app = express();
+
+// start server
+app.listen(3000, function(req, res){
+  console.log('Servidor está rodando');
+});
