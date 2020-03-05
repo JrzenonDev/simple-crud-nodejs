7ec093bdbe56d3827ebb377e790df6e8fcd68dad
JrzenonDev <jrobertoonb@gmail.com>
2020-03-05 00:09:29 -0300

    chore: criando a rota com render passando parâmetro id opcional e template com handlebars

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const handleBars = require('express-handlebars');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
+//template engine
+app.engine("handlebars", handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// routes and template
+app.get("/:id?", function(req, res) {
+  // ?: torna o id opcional
+  //res.send("Essa é minha página inicial");
+  //res.sendFile(__dirname+"/index.html");
+  res.render('index', {id:req.params.id});
+});
+
 // start server
-app.listen(3000, function(req, res){
+app.listen(3000, function(req, res) {
   console.log('Servidor está rodando');
 });
